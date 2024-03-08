8c392f310eead997ffdd28ac2f293d6339420aac
jayram10125 <jayram.10125@gmail.com>
2024-03-08 22:27:20 +0530

    calculating simple intrest

---
+++ PRACTISE QUESTION/calculatingSimpleIntrest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float p,r,t,si;
+    cout<<"enter principal amount : ";
+    cin>>p;
+    cout<<"enter rate value : ";
+    cin>>r;
+    cout<<"enter time : ";
+    cin>>t;
+    si=(p*r*t)/100;
+    cout<<"the intrest is : " <<si;
+
+
+}
