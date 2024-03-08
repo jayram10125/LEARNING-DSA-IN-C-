930f6065aac702259fb4636a043fcbb20671b352
jayram10125 <jayram.10125@gmail.com>
2024-03-08 21:47:42 +0530

    area of circle

---
+++ PRACTISE QUESTION/areaofACircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r,area;
+    cout<<"enter the radius of circle : ";
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"area of the circle is : "<<area;
+    return 0;
+}
