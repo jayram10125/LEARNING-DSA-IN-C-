0aa9fb4b54ebe98488d448c7371799f3097ee70f
jayram10125 <jayram.10125@gmail.com>
2024-03-08 23:02:29 +0530

    circumference of circle

---
+++ PRACTISE QUESTION/circumferenceOfACircleWithRadius10.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r=10;
+    float circumfrence;
+    circumfrence=2*3.14*r;
+    cout<<"circumfreace of the circle is : "<<circumfrence;
+    return 0;
+}
