5c76c55ad3fa3c9e2a7389bd930cf46a31afdef4
jayram10125 <jayram.10125@gmail.com>
2024-03-13 10:28:03 +0530

    adding new

---
+++ LOOPS IN C++/typesOfLoops.cpp
@@ -0,0 +1,19 @@
+
+// generally we use three types of loops
+// for (initializing loop veriable ; condition; updation)
+// {
+//     /* code */
+// }
+
+// while (/* condition */)
+// {
+//     /* code */
+// }
+
+// do
+// {
+//     /* code */
+// } while (/* condition */);
+
+
+
