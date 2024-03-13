d00fe2df39499f9e9a0fa9bced8c7deb37b712e0
jayram10125 <jayram.10125@gmail.com>
2024-03-13 13:49:51 +0530

    reverse ap printing till positive nnumber

---
+++ LOOPS IN C++/reverseAp.cpp
@@ -0,0 +1,16 @@
+// 100,97,94.... till positive number
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    int a=100;
+    int d=-3;
+    int an = a+(n-1)*d;
+    for(int i=100;i>=0;i=i-3)
+    {
+        cout<<" "<<i;
+    }
+    
+
+}
