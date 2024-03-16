11dcc793da478a2177e4898fe68f211f5207aa6b
jayram10125 <jayram.10125@gmail.com>
2024-03-16 20:55:23 +0530

    floyed triangle

---
+++ LOOPS IN C++/PATTERN PRINTING/PATTERN PRINTING 15 MARCH/floyedTriangle.cpp
@@ -0,0 +1,25 @@
+/*
+PRINT THE FLOYD TRIANGLE PATTERN
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,count =1;
+    cout<<"enter the size of floyed triangle : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+            {
+                cout<<count<<" ";
+                count++;
+            }
+            cout<<endl;
+
+    }
+}
