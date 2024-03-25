b6b2534404d169feb4238af25f6eef14417ad474
jayram10125 <jayram.10125@gmail.com>
2024-03-25 22:54:41 +0530

    pridict the output

---
+++ ARRAY/24MARCH/pridictTheOutput.cpp
@@ -0,0 +1,13 @@
+//Predict the output.
+#include<iostream>
+using namespace std;
+int main()
+{
+int sub[50], i ;
+for ( i = 0 ; i <= 48 ; i++ ) ;
+{
+sub[i] = i ;
+cout<<sub[i]<<endl ;//49
+}
+return 0;
+}
