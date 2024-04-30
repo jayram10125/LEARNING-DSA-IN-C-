3fc4329a324397932a6833fe2ba6e623c6ab1ea9
jayram10125 <jayram.10125@gmail.com>
2024-04-30 22:35:28 +0530

    printing some ASCII value

---
+++ STRING/asciiValue.cpp
@@ -0,0 +1,13 @@
+//ASCII value
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char ch='a' ,ch1='A',ch2='\0',ch3='@';
+    cout<<int(ch)<<endl;
+    cout<<int(ch1)<<endl;
+    cout<<int(ch2)<<endl;
+    cout<<int(ch3)<<endl;
+    return 0;
+}
