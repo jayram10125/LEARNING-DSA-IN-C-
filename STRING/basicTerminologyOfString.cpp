bdffac20456cd51bcb2bbee56beee42599818a46
jayram10125 <jayram.10125@gmail.com>
2024-04-30 22:05:36 +0530

    method of initializing string

---
+++ STRING/basicTerminologyOfString.cpp
@@ -0,0 +1,12 @@
+//basic terminology regarding string
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1 = "jay ram";  //method 1 to initialize the string
+    string str2("hello world");//method 2 to initialize the string
+    cout<<str1<<endl<<str2;
+    return 0;
+}
+
