c97531e6c56f8abc1448ba01cc032c128ba9f37e
jayram10125 <jayram.10125@gmail.com>
2024-05-01 14:13:48 +0530

    timecomplexity 1

---
+++ TIME AND SPACE COMPLEXITY/question1.cpp
@@ -0,0 +1,15 @@
+// Calculate the time complexity for iterating
+// in a loop.
+
+
+
+
+/*
+for(int i = 0; i < n; i++) {
+cout << "Alpha Intern\n";
+}
+*/
+
+
+
+//time complexity for worst case is O(n);
