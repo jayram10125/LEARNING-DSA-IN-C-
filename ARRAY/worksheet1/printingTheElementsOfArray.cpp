e87c77af1bc8ec34fea84a0e51f0840fb49144ec
jayram10125 <jayram.10125@gmail.com>
2024-07-15 20:51:52 +0530

    printing all thee elements of array

---
+++ ARRAY/worksheet1/printingTheElementsOfArray.cpp
@@ -0,0 +1,13 @@
+//Question 9: Write a program that prints all elements of an array int arr[4] = {5, 10, 15, 20};
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[4]={5,10,15,20};
+    for(int i=0;i<4;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
