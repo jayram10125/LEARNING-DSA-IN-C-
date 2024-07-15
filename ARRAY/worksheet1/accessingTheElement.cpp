36fc7a406bd83d33e7b2d3a2e1360d0b91b031ce
jayram10125 <jayram.10125@gmail.com>
2024-07-15 19:48:07 +0530

    access and print the element

---
+++ ARRAY/worksheet1/accessingTheElement.cpp
@@ -0,0 +1,12 @@
+//Question 6: Given an array int arr[5] = {10, 20, 30, 40, 50};, write the code to access and print the third element.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+    cout<<  "the third element is : " <<arr[2];
+
+
+
+    return 0;
+}
