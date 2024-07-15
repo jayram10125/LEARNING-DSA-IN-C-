d2fcf3b1d68c25843eafde0890165bc815e02267
jayram10125 <jayram.10125@gmail.com>
2024-07-15 20:14:06 +0530

    updating the element

---
+++ ARRAY/changeTheElementValue.cpp
@@ -0,0 +1,13 @@
+//Question 7: Change the second element of the array arr to 25 and print the updated array.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    arr[1]=25;
+    cout<<"the second updated element is : "<<arr[1];
+
+
+    return 0;
+}
