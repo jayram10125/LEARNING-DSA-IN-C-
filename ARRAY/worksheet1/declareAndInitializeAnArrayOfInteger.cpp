d92ddd6bc7e022792e43fd9a5bc0488eb12dd31a
jayram10125 <jayram.10125@gmail.com>
2024-07-15 19:41:18 +0530

    initilize and declare an integer array

---
+++ ARRAY/worksheet1/declareAndInitializeAnArrayOfInteger.cpp
@@ -0,0 +1,10 @@
+//Question 5: Declare and initialize an array of 4 integers with values 1, 2, 3, and 4 in one statement.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[4]={1,2,3,4};
+
+
+    return 0;
+}
