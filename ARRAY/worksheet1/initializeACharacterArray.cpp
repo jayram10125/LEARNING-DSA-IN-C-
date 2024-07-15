ae56b07d00b5a2449fa7a375de0bee8362957bbc
jayram10125 <jayram.10125@gmail.com>
2024-07-15 19:35:04 +0530

    initialize array of character

---
+++ ARRAY/worksheet1/initializeACharacterArray.cpp
@@ -0,0 +1,9 @@
+//Question 4: Initialize an array of 3 characters with values 'a', 'b', and 'c'.
+#include<iostream>
+using namespace std;
+int main()
+{
+    char arr[3]={'a','b','c'};
+
+    return 0;
+}
