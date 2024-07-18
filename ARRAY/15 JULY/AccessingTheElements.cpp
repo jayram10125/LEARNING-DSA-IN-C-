6f3a73f82c8561a7c47bec0291747ecbca1ddd98
jayram10125 <jayram.10125@gmail.com>
2024-07-18 16:08:50 +0530

    accessing the element

---
+++ ARRAY/15 JULY/AccessingTheElements.cpp
@@ -0,0 +1,20 @@
+//accessing the elements
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={5,6,7,3,9};
+    cout<<arr[0]<<" ";
+    cout<<arr[1]<<" ";
+    cout<<arr[2]<<" ";
+    cout<<arr[3]<<" ";
+    cout<<arr[4]<<" ";
+//OR
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;  
+} 
