62ff718daa5ff95b578c9840776625c0a07d1b1e
jayram10125 <jayram.10125@gmail.com>
2024-07-18 16:21:37 +0530

    address of array and its elements

---
+++ ARRAY/15 JULY/memeoyLocation.cpp
@@ -0,0 +1,18 @@
+//address of array 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10]={1,2,5,9,3,8};
+    cout<<arr<<endl;
+    //(cout<<&arr<<endl;)
+    cout<<&arr[0]<<endl;
+    cout<<&arr[1]<<endl;
+    cout<<&arr[2]<<endl;
+    cout<<&arr[3]<<endl;
+    cout<<&arr[4]<<endl;
+
+
+
+    return 0;  
+} 
