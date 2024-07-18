d01d9475580ccbf56633732cda9e3e42133f023f
jayram10125 <jayram.10125@gmail.com>
2024-07-18 16:17:37 +0530

    size of operator

---
+++ ARRAY/15 JULY/sizeOfOperator.cpp
@@ -0,0 +1,13 @@
+//size of operator
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10]={1,2,3,4,5,6};
+    cout<<"size of array is : "<< sizeof(arr);
+    cout<<endl;
+    cout<<"size of each element of intiger array : "<<sizeof(arr[1]);
+
+
+    return 0;  
+} 
