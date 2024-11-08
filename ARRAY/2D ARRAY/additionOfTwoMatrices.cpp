20b0e878465eb9c66b0e4a6d6d096e5f9bb33a56
jayram10125 <jayram.10125@gmail.com>
2024-11-08 11:35:01 +0530

    addition and subtraction of two matrices

--- ARRAY/2D ARRAY/additionOfTwoMatrices.cpp
+++ ARRAY/2D ARRAY/additionOfTwoMatrices.cpp
@@ -1,11 +1,12 @@
-//addition of two matrices
+//addition and subtraction  of two matrices
 #include<iostream>
 using namespace std;
 int main()
 {
     int matrix1[3][3];
     int matrix2[3][3];
-    int result[3][3];
+    int subresult[3][3];
+    int addresult[3][3];
     cout<<"enter the elements of first matrix : \n";
     for(int i=0;i<3;i++)
     {
@@ -22,14 +23,23 @@ int main()
             cin>>matrix2[i][j];
         }
     }
-
+//addition
     for(int i=0;i<3;i++)
     {
         for(int j=0;j<3;j++)
         {
-            result[i][j]=matrix1[i][j]+matrix2[i][j];
+            addresult[i][j]=matrix1[i][j]+matrix2[i][j];
         }
     }
+//subtraction
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            subresult[i][j]=matrix1[i][j]-matrix2[i][j];
+        }
+    }
+
     cout<<"first matrix is  : \n";
     for(int i=0;i<3;i++)
     {
@@ -54,7 +64,17 @@ int main()
     {
         for(int j=0;j<3;j++)
         {
-            cout<<result[i][j]<<" ";
+            cout<<addresult[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"the difference of matrix1 and matrix 2 is : \n";
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cout<<subresult[i][j]<<" ";
         }
         cout<<endl;
     }
